250427564cde7b2d0278cd48038cb193dfbd30ec
gabrielnemeth <github.zaphrentid@aleeas.com>
2022-01-01 18:17:12 +0100

    Configure linters and formatters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,61 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'import', 'unused-imports'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+            },
+        ],
+        '@typescript-eslint/array-type': [
+            'error',
+            {
+                default: 'array',
+            },
+        ],
+        '@typescript-eslint/consistent-type-definitions': [
+            'error',
+            'interface',
+        ],
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        '@typescript-eslint/explicit-member-accessibility': [
+            'error',
+            {
+                accessibility: 'explicit',
+            },
+        ],
+        '@typescript-eslint/method-signature-style': ['error', 'method'],
+        '@typescript-eslint/no-for-in-array': 'error',
+        '@typescript-eslint/no-inferrable-types': ['off'],
+        'default-case': 'warn',
+        'no-template-curly-in-string': 'error',
+        'prefer-template': 'error',
+        'import/order': [
+            'error',
+            {
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+                groups: [['external', 'builtin'], 'parent', 'sibling', 'index'],
+            },
+        ],
+        'unused-imports/no-unused-imports': 'error',
+    },
 };
